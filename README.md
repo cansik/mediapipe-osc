4c308bb7b9624fdbb50bc4fc963002a197f77005
Florian Bruggisser <florian@bildspur.ch>
2020-12-08 11:55:27 +0100

    added very basic example of pose osc sending

--- README.md
+++ README.md
@@ -9,8 +9,18 @@ Currently only MacOS and Linux are supported. It's recommended to use Python3 an
 python install -r requirements.txt
 ```
 
-### Single Human Pose
-tbd
+### Pose Landmark Model (BlazePose Tracker)
+The landmark model currently included in MediaPipe Pose predicts the location of 25 upper-body landmarks (see figure below), each with (`x, y, z, visibility`). Note that the z value should be discarded as the model is currently not fully trained to predict depth, but this is something we have on the roadmap. The model shares the same architecture as the full-body version that predicts 33 landmarks, described in more detail in the [BlazePose Google AI Blog](https://ai.googleblog.com/2020/08/on-device-real-time-body-pose-tracking.html) and in this [paper](https://arxiv.org/abs/2006.10204).
+
+![Pose Description](readme/pose_tracking_upper_body_landmarks.png)
+
+*[Reference: mediapipe/solutions/pose](https://google.github.io/mediapipe/solutions/pose#pose-landmark-model-blazepose-tracker)*
+
+The OSC message sent by the pose example contains 25 x 4 `float` values.
+
+```
+/mediapipe/pose [x, y, z, visibility, x, y, z, visibility ...]
+```
 
 ### Hand Detection
 tbd
--- pose.py
+++ pose.py
@@ -1,32 +1,80 @@
+import argparse
+from typing import List, Tuple
+
 import cv2
 import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+from pythonosc import udp_client
+from pythonosc.osc_message_builder import OscMessageBuilder
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+OSC_ADDRESS = "/mediapipe/pose"
 
-pose = mp_pose.Pose(
-    min_detection_confidence=0.5, min_tracking_confidence=0.5)
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-        break
 
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    results = pose.process(image)
+def send_pose(client: udp_client,
+              landmark_list: landmark_pb2.NormalizedLandmarkList):
+    if landmark_list is None:
+        return
 
-    # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-    cv2.imshow('MediaPipe Pose', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-        break
-pose.close()
-cap.release()
+    # create message and send
+    builder = OscMessageBuilder(address=OSC_ADDRESS)
+    for landmark in landmark_list.landmark:
+        builder.add_arg(landmark.x)
+        builder.add_arg(landmark.y)
+        builder.add_arg(landmark.z)
+        builder.add_arg(landmark.visibility)
+    msg = builder.build()
+    client.send(msg)
+
+
+def main():
+    # read arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=int, default=0,
+                        help="The id of the capture device (camera)")
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=7500,
+                        help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    # create osc client
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+    # setup camera loop
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
+    # todo: expose confidence parameters
+    pose = mp_pose.Pose(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    cap = cv2.VideoCapture(args.device)
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            break
+
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = pose.process(image)
+
+        # send the pose over osc
+        send_pose(client, results.pose_landmarks)
+
+        # Draw the pose annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        cv2.imshow('MediaPipe OSC Pose', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+    pose.close()
+    cap.release()
+
+
+if __name__ == "__main__":
+    main()

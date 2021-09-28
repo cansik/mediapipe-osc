5de9714523037d2c8dd27fde66d2bcc2258bd0b8
Florian Bruggisser <florian@bildspur.ch>
2021-09-28 12:46:13 +0200

    fixed facemesh to work with 0.8.7.3

--- facemesh.py
+++ facemesh.py
@@ -29,7 +29,7 @@ while cap.isOpened():
             mp_drawing.draw_landmarks(
                 image=image,
                 landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACE_CONNECTIONS,
+                connections=mp_face_mesh.FACEMESH_CONTOURS,
                 landmark_drawing_spec=drawing_spec,
                 connection_drawing_spec=drawing_spec)
     cv2.imshow('MediaPipe FaceMesh', image)
